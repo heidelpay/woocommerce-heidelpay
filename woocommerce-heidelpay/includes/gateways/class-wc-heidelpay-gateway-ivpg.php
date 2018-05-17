1d83b98bcfdd3bbf6b66a2ff82d7db16b6bbe434
Florian Evertz <florian.evertz@heidelpay.de>
2018-05-17 10:58:33 +0200

    Add Email text Support

--- woocommerce-heidelpay/includes/gateways/class-wc-heidelpay-gateway-ivpg.php
+++ woocommerce-heidelpay/includes/gateways/class-wc-heidelpay-gateway-ivpg.php
@@ -17,6 +17,13 @@ class WC_Gateway_HP_IVPG extends WC_Heidelpay_Payment_Gateway
     /** @var array Array of locales */
     public $locale;
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        add_action('woocommerce_email_before_order_table', array($this, 'email_instructions'), 10, 3);
+    }
+
     /**
      * Initialise Gateway Settings Form Fields.
      */
@@ -27,6 +34,7 @@ class WC_Gateway_HP_IVPG extends WC_Heidelpay_Payment_Gateway
         $title = 'Secured Invoice';
         $this->form_fields['title']['default'] = __($title, 'woocommerce-heidelpay');
         $this->form_fields['description']['default'] = __('Insert payment data for ' . $title, 'woocommerce-heidelpay');
+        $this->form_fields['instructions']['default'] = __('Bitte überweisen sie den Betrag innerhalb der nächsten 14 Tage an Folgendes Konto: EMPFÄNGER IBAN BIC', 'woocommerce-heidelpay');
         $this->form_fields['enabled']['label'] = __('Enable ' . $title, 'woocommerce-heidelpay');
         $this->form_fields['security_sender']['default'] = '31HA07BC8142C5A171745D00AD63D182';
         $this->form_fields['user_login']['default'] = '31ha07bc8142c5a171744e5aef11ffd3';
@@ -59,6 +67,14 @@ class WC_Gateway_HP_IVPG extends WC_Heidelpay_Payment_Gateway
     {
         //callback stuff
     }
+
+    public function email_instructions($order, $sent_to_admin, $plain_text = false)
+    {
+        if ($this->instructions) {
+            echo wpautop(wptexturize($this->instructions)) . PHP_EOL;
+        }
+    }
+
 
     /**
      * Set the id and PaymenMethod
