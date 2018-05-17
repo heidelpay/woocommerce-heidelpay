b4385bf7958918ff8f46581506d85cb76880a544
Florian Evertz <florian.evertz@heidelpay.de>
2018-05-17 11:34:54 +0200

    change DD to abstract child class update abstract gateway

--- woocommerce-heidelpay/includes/gateways/class-wc-heidelpay-gateway-dd.php
+++ woocommerce-heidelpay/includes/gateways/class-wc-heidelpay-gateway-dd.php
@@ -1,216 +1,97 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
 /**
  * Direct debit
  */
-require_once ( dirname(__DIR__) . '../../vendor/autoload.php');
+require_once(dirname(__DIR__) . '../../vendor/autoload.php');
+require_once(WC_HEIDELPAY_PLUGIN_PATH . '/includes/abstracts/abstract-wc-heidelpay-payment-gateway.php');
+
 use Heidelpay\PhpPaymentApi\PaymentMethods\DirectDebitPaymentMethod;
 
-class WC_Gateway_HP_DD extends WC_Payment_Gateway {
-
+class WC_Gateway_HP_DD extends WC_Heidelpay_Payment_Gateway
+{
     /** @var array Array of locales */
     public $locale;
 
     /**
-     * Constructor for the gateway.
-     */
-    public function __construct() {
-
-        $this->DirectDebit = new DirectDebitPaymentMethod();
-
-        $this->id                 = 'hp_dd';
-        //$this->icon               = apply_filters( 'hp_dd_icon', '' );
-        $this->has_fields         = true;
-        $this->method_title       = __( 'HP_DD', 'woocommerce-heidelpay' );
-        $this->method_description = __( 'heidelpay direct debit', 'woocommerce-heidelpay' );
-
-        // Load the settings.
-        $this->init_form_fields();
-        $this->init_settings();
-
-        // Define user set variables
-        $this->title        = $this->get_option( 'title' );
-        $this->description  = $this->get_option( 'description' );
-        $this->instructions = $this->get_option( 'instructions' );
-
-        // Actions
-        //oupdate option values
-        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-        //callback handler for response
-        add_action( 'woocommerce_api_' . $this->id, array( $this, 'callback_handler' ) );
-        /*add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'save_account_details' ) );
-        add_action( 'woocommerce_thankyou_hp_dd', array( $this, 'thankyou_page' ) );
-
-        // Customer Emails
-        add_action( 'woocommerce_email_before_order_table', array( $this, 'email_instructions' ), 10, 3 );*/
-    }
-
-    /**
      * Initialise Gateway Settings Form Fields.
      */
-    public function init_form_fields() {
+    public function init_form_fields()
+    {
+        parent::init_form_fields();
 
-        $this->form_fields = array(
-            'enabled' => array(
-                'title'   => __( 'Enable/Disable', 'woocommerce-heidelpay' ),
-                'type'    => 'checkbox',
-                'label'   => __( 'Enable direct debit', 'woocommerce-heidelpay' ),
-                'default' => 'yes',
-            ),
-            'title' => array(
-                'title'       => __( 'Title', 'woocommerce-heidelpay' ),
-                'type'        => 'text',
-                'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce-heidelpay' ),
-                'default'     => __( 'direct debit', 'woocommerce-heidelpay' ),
-                'desc_tip'    => true,
-            ),
-            'description' => array(
-                'title'       => __( 'Description', 'woocommerce-heidelpay' ),
-                'type'        => 'textarea',
-                'description' => __( 'Payment method description that the customer will see on your checkout.', 'woocommerce-heidelpay' ),
-                'default'     => __( 'Insert payment data for direct debit', 'woocommerce-heidelpay' ),
-                'desc_tip'    => true,
-            ),
-            'instructions' => array(
-                'title'       => __( 'Instructions', 'woocommerce-heidelpay' ),
-                'type'        => 'textarea',
-                'description' => __( 'Instructions that will be added to the thank you page and emails.', 'woocommerce-heidelpay' ),
-                'default'     => 'The following acount will be billed:',
-                'desc_tip'    => true,
-            ),
-            'security_sender' => array(
-                'title'       => __( 'Security Sender', 'woocommerce-heidelpay' ),
-                'type' => 'text',
-                'id' => 'hp_dd_security_sender',
-                'description' => 'Security Sender',
-                'default' => '31HA07BC8142C5A171745D00AD63D182',
-            ),
-            'user_login' => array(
-                'title'       => __( 'User Login', 'woocommerce-heidelpay' ),
-                'type' => 'text',
-                'id' => 'hp_dd_user_login',
-                'description' => 'User Login',
-                'default' => '31ha07bc8142c5a171744e5aef11ffd3',
-            ),
-            'user_password' => array(
-                'title'       => __( 'User Password', 'woocommerce-heidelpay' ),
-                'type' => 'text',
-                'id' => 'hp_dd_user_password',
-                'description' => 'User Password',
-                'default' => '93167DE7',
-            ),
-            'transaction_channel' => array(
-                'title'       => __( 'Transaction Channel', 'woocommerce-heidelpay' ),
-                'type' => 'text',
-                'id' => 'hp_dd_transaction_channel',
-                'description' => 'Transaction Channel',
-                'default' => '31HA07BC8142C5A171744F3D6D155865',
-            ),
-            'sandbox' => array(
-                'title'   => __( 'Sandbox', 'woocommerce-heidelpay' ),
-                'type'    => 'checkbox',
-                'id' => 'hp_dd_sandbox',
-                'label'   => __( 'Enable sandbox mode', 'woocommerce-heidelpay' ),
-                'default' => 'yes',
-            ),
-        );
+        $this->form_fields['description']['default'] = __('Insert payment data for'
+            . $this->name, 'woocommerce-heidelpay');
+        $this->form_fields['title']['default'] = __($this->name, 'woocommerce-heidelpay');
+        $this->form_fields['security_sender']['default'] = '31HA07BC8142C5A171745D00AD63D182';
+        $this->form_fields['user_login']['default'] = '31ha07bc8142c5a171744e5aef11ffd3';
+        $this->form_fields['user_password']['default'] = '93167DE7';
+        $this->form_fields['transaction_channel']['default'] = '31HA07BC8142C5A171744F3D6D155865';
     }
 
-    public function admin_options() {
-        ?>
-        <h2><?php _e('heidelpay DD','woocommerce'); ?></h2>
-        <table class="form-table">
-            <?php $this->generate_settings_html(); ?>
-        </table> <?php
-    }
-
-    //payment form
-    public function payment_fields() {
+    public function payment_fields()
+    {
         echo '<div>';
 
-        echo
-        'Holder:<input type="text" name="ACCOUNT.HOLDER" value="" /><br/>
-            IBan:<input type="text" name="ACCOUNT.IBAN" value="" /><br/>'
-        ;
+        echo 'Holder:<input type="text" name="ACCOUNT.HOLDER" value="" /><br/>
+            IBan:<input type="text" name="ACCOUNT.IBAN" value="" /><br/>';
 
         echo '</div>';
     }
 
+    //payment form
+
     /**
-     * Process the payment and return the result.
-     *
-     * @param int $order_id
-     * @return array
+     * Set the id and PaymenMethod
      */
-    public function process_payment( $order_id ) {
-        $order = wc_get_order( $order_id );
-
-        // Mark as on-hold (we're awaiting the payment)
-        $order->update_status( 'on-hold', __( 'Awaiting HP_DD payment', 'woocommerce-heidelpay' ) );
-
-        // Reduce stock levels
-        wc_reduce_stock_levels( $order_id );
-
-        // Remove cart
-        WC()->cart->empty_cart();
-
-        /**
-         * Set up your authentification data for Heidepay api
-         */
-        $this->DirectDebit->getRequest()->authentification(
-            $this->settings('hp_dd_security_sender'),  // SecuritySender
-            $this->settings('hp_dd_user_login'),  // UserLogin
-            $this->settings('hp_dd_user_password'),                         // UserPassword
-            $this->settings('hp_dd_transaction_channel'),  // TransactionChannel
-            $this->settings('hp_dd_sandbox')                                 // Enable sandbox mode
-        );
-        /**
-         * Set up asynchronous request parameters
-         */
-        $this->DirectDebit->getRequest()->async(
-            'https://www.google.de'
-        );
-
-        /**
-         * Set up customer information required for risk checks
-         */
-
-        $this->DirectDebit->getRequest()->customerAddress(
-            $order->get_billing_first_name(),                  // Given name
-            $order->get_billing_last_name(),           // Family name
-            $order->get_billing_company(),                     // Company Name
-            $order->get_customer_id(),                   // Customer id of your application
-            $order->get_billing_address_1() . $order->get_billing_address_2(),          // Billing address street
-            $order->get_billing_state(),                   // Billing address state
-            $order->get_billing_postcode(),                   // Billing address post code
-            $order->get_billing_city(),              // Billing address city
-            $order->get_billing_country(),                      // Billing address country code
-            $order->get_billing_email()     // Customer mail address
-        );
-
-        /**
-         * Set up basket or transaction information
-         */
-        $this->DirectDebit->getRequest()->basketData(
-            $order_id, //order id
-            wc()->cart->calculate_totals(),                         //cart amount
-            'EUR',                         // Currency code of this request
-            'secret'    // A secret passphrase from your application
-        );
-
-        /**
-         * Set necessary parameters for Heidelpay payment Frame and send a registration request
-         */
-        $this->DirectDebit->debit();
+    protected function setPayMethod()
+    {
+        $this->payMethod = new DirectDebitPaymentMethod();
+        $this->id = 'hp_dd';
+        $this->has_fields = true;
+        $this->name = 'Direct Debit';
+        $this->method_title = __('HP_DD', 'woocommerce-heidelpay');
+        $this->method_description = __('heidelpay direct debit', 'woocommerce-heidelpay');
     }
 
-    public function callback_handler() {
-        /*
-         * differentation between response and push handled in the callback handler
-         */
+    /**
+     * Send payment request
+     * @return mixed
+     */
+    protected function performRequest()
+    {
+        $logger = wc_get_logger();
+        try {
+            $this->payMethod->debit();
+        } catch (\Exception $exception) {
+            $logger->log(WC_Log_Levels::DEBUG, print_r('Paymethod not found', 1));
+            // TODO: redirect to errorpage
+        }
+
+        //logging and debug
+        $logger = wc_get_logger();
+        $logger->log(WC_Log_Levels::DEBUG, print_r($this->payMethod->getRequest(), 1));
+        $logger->log(
+            WC_Log_Levels::DEBUG,
+            print_r(get_permalink(wc_get_page_id('shop')) . 'wc-api' . strtolower(get_class($this)), 1)
+        );
+
+        if ($this->payMethod->getResponse()->isSuccess()) {
+            return [
+                'result' => 'success',
+                'redirect' => $this->payMethod->getResponse()->getPaymentFormUrl()
+            ];
+        }
+
+        wc_add_notice(
+            __('Payment error: ', 'woothemes') . $this->payMethod->getResponse()->getError()['message'],
+            'error'
+        );
+
+        return null;
     }
 }
