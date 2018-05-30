6ee98dbab20c527e97d282dabbe074ff45edb095
Florian Evertz <florian.evertz@heidelpay.de>
2018-05-30 13:22:13 +0200

    add javascript

---
+++ includes/js/securedInvoice.js
@@ -0,0 +1,14 @@
+/**
+ * SUMMARY
+ *
+ * DESC
+ *
+ * @license Use of this software requires acceptance of the License Agreement. See LICENSE file.
+ * @copyright Copyright © 2016-present heidelpay GmbH. All rights reserved.
+ * @author Florian Evertz
+ * @category WOOCOMMERCE
+ */
+
+function(
+
+);
