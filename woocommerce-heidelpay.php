9ea34325619e5d728214c5e9294d27acc726328a
Florian Evertz <florian.evertz@heidelpay.de>
2019-11-19 14:31:53 +0100

    [Version] Change Version

--- woocommerce-heidelpay.php
+++ woocommerce-heidelpay.php
@@ -10,7 +10,7 @@ use Heidelpay\MessageCodeMapper\MessageCodeMapper;
  * Plugin Name: heidelpay WooCommerce
  * Plugin URI: https://dev.heidelpay.com
  * Description: heidelpay payment integration for WooCommerce
- * Version: 1.6.0
+ * Version: 1.7.0
  * Author: heidelpay
  * Author URI: htts://www.heidelpay.com
  * Developer: heidelpay
@@ -29,7 +29,7 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
     /**
      * Required minimums and constants
      */
-    define('WC_HEIDELPAY_VERSION', '1.6.0');
+    define('WC_HEIDELPAY_VERSION', '1.7.0');
     define('WC_HEIDELPAY_MIN_PHP_VER', '5.6.0');
     define('WC_HEIDELPAY_MIN_WC_VER', '3.0.0');
     define('WC_HEIDELPAY_MAIN_FILE', __FILE__);
@@ -40,7 +40,6 @@ if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_
 
         class WC_Heidelpay
         {
-
             /**
              * @var Singleton The reference the *Singleton* instance of this class
              */
